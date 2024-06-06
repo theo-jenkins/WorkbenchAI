46564b3d89eac003c619f9f502c461085ed5b580
Theo J <theo@gmail.com>
2024-06-06 10:23:50 +0100

    Setting up the while loop for user input

---
+++ nn_sandbox_script.py
@@ -0,0 +1,8 @@
+while status == True:
+    print('This is your neural network dashboard.')#
+    print('[1] - Load data')
+    print('[2] - Exit')
+    answer = input()
+
+    if int(answer) == 2:
+        status = False
