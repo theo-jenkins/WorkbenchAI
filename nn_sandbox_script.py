0bf57e138ced269a40b69436eb9a8fcc26486764
Theo J <theo@gmail.com>
2024-06-13 14:31:47 +0100

    "Removed the dictionary datatype for the dataset variable"

--- nn_sandbox_script.py
+++ nn_sandbox_script.py
@@ -1,5 +1,8 @@
-from keras import layers, models
+import tensorflow as tf
 import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
 
 def greeting():
     """
@@ -10,53 +13,64 @@ def greeting():
     """
 
     print("This is your neural network dashboard.")
-    print("[1] - Train keras NN")
-    print("[2] - Exit")
+    print("[1] - Prepare data")
+    print("[2] - Build model")
+    print("[3] - Train model")
+    print("[4] - Exit")
 
     while True:
         try:
             answer = int(input("Enter your choice:"))
-            if answer in [1,2]:
+            if answer in [1,2,3,4]:
                 return answer
             else:
-                print("Please enter 1 or 2")
+                print("Please enter a valid number.")
         except ValueError:
-            print("Invalid input: Please enter a valid number")
+            print("Invalid input: Please enter a valid number.")
 
-def keras_function():
-    from keras.datasets import boston_housing
+def prepare_data():
+    #Load data (mnist digit dataset)
+    #x refers to images, y refers to their labels
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-    (train_data, train_targets), (test_data, test_targets) = boston_housing.load_data()
-    mean = train_data.mean(axis=0)
-    train_data -= mean
-    std = train_data.std(axis=0)
-    train_data /= std
-    
-    test_data -= mean
-    test_data /= std
+    #Normalize the data (between 0 - 1)
+    x_train, x_test = x_train / 255.0, x_test / 255.0
 
-    input_shape = train_data.shape[1]
-    model = build_model(input_shape)
-    model.fit(train_data, train_targets, epochs=80, batch_size=16, verbose=0)
-    test_mse_score, test_mae_score = model.evaluate(test_data, test_targets)
-    print(f"test_mse_score = {test_mse_score}, test_mae_score = {test_mae_score}")
+    print(f"Data prepared: {x_train[1]}, label: {y_train[1]}")
+    return x_train, y_train, x_test, y_test
 
-def build_model(input_shape):
-    model = models.Sequential()
-    model.add(layers.Dense(64, activation='relu', input_shape=(input_shape,)))
-    model.add(layers.Dense(64, activation='relu'))
-    model.add(layers.Dense(1))
+def build_model(x_train):
+    #Outlines the architecture of the neural network
+    #Returns: neural net model
+    input_shape = x_train.shape[1]
+
+    model = Sequential()
+    model.add(Flatten(input_shape=(28,28)))
+    model.add(Dense(16, activation='relu'))
+    model.add(Dense(1))
     model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
 
+    print(f"Model built")
     return model
+
+def train_model(x_train, y_train, x_test, y_test, model):
+    model.fit(x_train, y_train, epochs=4, batch_size=512)
+    results = model.evaluate(x_test, y_test)
+    print(f"results {results}")
+    return results
 
 def main():
     user_choice = 0
-    while user_choice != 2:
+    while user_choice != 4:
         user_choice = greeting()
 
         if user_choice == 1:
-            keras_function()
+            x_train, y_train, x_test, y_test = prepare_data()
+        if user_choice == 2:
+            model = build_model(x_train)
+        if user_choice == 3:
+            train_model(x_train, y_train, x_test, y_test, model)
+        
     
 
 
